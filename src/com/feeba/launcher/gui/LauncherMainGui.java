649333def50466e3bd49c27cef02bd5ca4252396
Sebastian <sebastian_spanner@hotmail.com>
2013-07-26 16:10:09 +0200

    initial commit

---
+++ src/com/feeba/launcher/gui/LauncherMainGui.java
@@ -0,0 +1,160 @@
+package com.feeba.launcher.gui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import java.awt.Color;
+import java.awt.Cursor;
+import java.awt.Font;
+
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class LauncherMainGui extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					LauncherMainGui frame = new LauncherMainGui();
+					frame.setLocationRelativeTo(null);
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public LauncherMainGui() {
+		setBackground(new Color(0x17748F));
+		setTitle("Feeba");
+		setResizable(false);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 649, 450);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(0, 0, 0, 0));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		final JPanel loadButton = new JPanel();
+		loadButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				// add loading action
+			}
+			
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				enter(loadButton);
+				}
+			
+			@Override
+			public void mouseExited(MouseEvent e) {
+				exit(loadButton);
+			}
+		});
+		
+		loadButton.setBackground(Color.WHITE);
+		loadButton.setBounds(371, 28, 245, 40);
+		contentPane.add(loadButton);
+		
+		JLabel loadButtonText = new JLabel("Umfrage laden");
+		loadButtonText.setFont(new Font("Manteka", Font.PLAIN, 25));
+		loadButton.add(loadButtonText);
+		
+		final JPanel newButton = new JPanel();
+		newButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				// add new action
+			}
+			
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				enter(newButton);
+			}
+			
+			@Override
+			public void mouseExited(MouseEvent e) {
+				exit(newButton);
+			}
+		});
+		
+		newButton.setBackground(Color.WHITE);
+		newButton.setBounds(237, 91, 379, 40);
+		contentPane.add(newButton);
+		
+		JLabel newButtonText = new JLabel("Neue Umfrage erstellen");
+		newButtonText.setFont(new Font("Manteka", Font.PLAIN, 25));
+		newButton.add(newButtonText);
+		
+		final JPanel resultButton = new JPanel();
+		resultButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				
+				// add analyse action
+			}
+			
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				enter(resultButton);
+			}
+			
+			@Override
+			public void mouseExited(MouseEvent e) {
+				exit(resultButton);
+			}
+		});
+		
+		resultButton.setBackground(Color.WHITE);
+		resultButton.setBounds(268, 154, 352, 40);
+		contentPane.add(resultButton);
+		
+		JLabel resultButtonText = new JLabel("Umfrage auswerten");
+		resultButtonText.setFont(new Font("Manteka", Font.PLAIN, 25));
+		resultButton.add(resultButtonText);
+		
+		JLabel Bars = new JLabel("");
+		Bars.setBounds(0, 206, 1280, 229);
+		contentPane.add(Bars);
+		Bars.setIcon(new ImageIcon(LauncherMainGui.class.getResource("/images/bars.png")));
+	}
+	
+	public void enter(JPanel panel) {
+		
+		panel.setBackground(Color.LIGHT_GRAY);
+		Cursor cursor = Cursor.getPredefinedCursor(Cursor.HAND_CURSOR); 
+	    setCursor(cursor);
+		
+		
+	}
+	
+	public void exit(JPanel panel) {
+		
+		panel.setBackground(Color.WHITE);
+		Cursor cursor = Cursor.getDefaultCursor();
+	    setCursor(cursor);
+		
+	}
+	
+}
