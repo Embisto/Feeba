521469a2faec24c19db07b0eb55c48ee3ea6de52
Sebastian <sebastian_spanner@hotmail.com>
2013-09-15 19:57:56 +0200

    changed data

--- src/com/feeba/data/DataTester.java
+++ src/com/feeba/data/DataTester.java
@@ -8,11 +8,11 @@ public class DataTester {
 	public static void main(String[] args) {
 		
 		Survey s1 = new Survey("Fragebogen 1");
-		Question ques1 = new Question("Frage 1", "Wie alt bist du?", QuestionType.FREETEXT);
-		Question ques2 = new Question("Frage 2","hast du schon einmal gesungen?",QuestionType.SINGLE_SELECTION);
-		Question ques3 = new Question("Frage 3", "Wieviel is 2*3?",QuestionType.MULTIPLE_CHOICE);
-		Question ques4 = new Question("Frage 4", "Haus?", QuestionType.FREETEXT);
-		Question ques5 = new Question("Frage 5", "Elefant?", QuestionType.FREETEXT);
+		Question ques1 = new Question("Alter", "Wie alt bist du?", QuestionType.FREETEXT);
+		Question ques2 = new Question("Gesang","hast du schon einmal gesungen?",QuestionType.SINGLE_SELECTION);
+		Question ques3 = new Question("Mathe", "Wieviel is 2*3?",QuestionType.MULTIPLE_CHOICE);
+		Question ques4 = new Question("Architektur", "Haus?", QuestionType.FREETEXT);
+		Question ques5 = new Question("Tiere", "Elefant?", QuestionType.FREETEXT);
 		ques3.addChoice("2", "3","6","9");
 		ques2.addChoice("Ja","Nein");
 		s1.addQuestion(ques1,ques2,ques3,ques4,ques5);
